65c9d354544bdea6fc565567ac3f6498dfc68815
Gefferson Casasola <gcasasolah@uni.pe>
2024-08-31 12:06:25 -0500

    Feat lifespan error management

--- bot/ptb.py
+++ bot/ptb.py
@@ -21,16 +21,18 @@ else:
 async def lifespan(_: FastAPI):
     try:
         if config.BOTHOST:
+            print(f"Attempting to configure webhook with URL: {config.BOTHOST}")
             webhook_info = await ptb.bot.get_webhook_info()
             if webhook_info.url != config.BOTHOST:
-                await ptb.bot.setWebhook(config.BOTHOST)
+                await ptb.bot.setWebhook(config.BOTHOST.strip())
                 print(f"Webhook configured at {config.BOTHOST}")
             else:
                 print("Webhook already configured")
         async with ptb:
             await ptb.start()
             yield
-            await ptb.stop()
     except Exception as e:
         print(f"An error occurred during the lifespan management: {e}")
         traceback.print_exc()
+    finally:
+        await ptb.stop()
