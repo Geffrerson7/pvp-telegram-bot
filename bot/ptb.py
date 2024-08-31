439574ba9785721958308534d4c22f60561ad1b1
Gefferson Casasola <gcasasolah@uni.pe>
2024-08-31 11:52:28 -0500

    Feat lifespan error management

--- bot/ptb.py
+++ bot/ptb.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from settings import config
 from telegram.ext import Application
+import traceback
 
 
 ptb = (
@@ -18,14 +19,18 @@ else:
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    if config.BOTHOST:
-        webhook_info = await ptb.bot.get_webhook_info()
-        if webhook_info.url != config.BOTHOST:
-            await ptb.bot.setWebhook(config.BOTHOST)
-            print(f"Webhook configured at {config.BOTHOST}")
-        else:
-            print("Webhook already configured")
-    async with ptb:
-        await ptb.start()
-        yield
-        await ptb.stop()
+    try:
+        if config.BOTHOST:
+            webhook_info = await ptb.bot.get_webhook_info()
+            if webhook_info.url != config.BOTHOST:
+                await ptb.bot.setWebhook(config.BOTHOST)
+                print(f"Webhook configured at {config.BOTHOST}")
+            else:
+                print("Webhook already configured")
+        async with ptb:
+            await ptb.start()
+            yield
+            await ptb.stop()
+    except Exception as e:
+        print(f"An error occurred during the lifespan management: {e}")
+        traceback.print_exc()
